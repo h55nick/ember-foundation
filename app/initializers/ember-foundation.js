cba4fe05e476afe9d366ef4a665d186d2cffbf84
Blanchet <me@nblanchet.com>
2014-12-03 15:43:24 -0500

    ensure foundation is only run once per run loop.

--- app/initializers/ember-foundation.js
+++ app/initializers/ember-foundation.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
+var isFoundationDirty = false;
+
 export default {
   name: 'ember-foundation',
 
@@ -7,9 +9,18 @@ export default {
     app.inject('component:f-breadcrumbs', 'router', 'router:main');
 
     Ember.View.reopen({
-      initFoundation: function() {
-        Ember.$(document).foundation();
-      }.on('didInsertElement')
+      _initFoundation: function() {
+
+        isFoundationDirty = true;
+        Ember.run.schedule('afterRender', this, this._updateFoundation);
+      }.on('didInsertElement'),
+
+      _updateFoundation: function(){
+        if(isFoundationDirty){
+          Ember.$.fn.foundation(document.body);
+          isFoundationDirty = false;
+        }
+      }
     });
   }
 };
